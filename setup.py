77a7a0a1389173eb0bc514443776f7c07e52d36d
SincereXIA <937734121@qq.com>
2018-08-23 19:23:01 +0800

    fix setup

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description='An asynchronous QQ bot framework based on CoolQ.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    install_requires=['aiocqhttp>=0.6', 'aiocache>=0.10', 'apscheduler', 'sqlalchemy', 'aip', 'jieba'],
+    install_requires=['aiocqhttp>=0.6', 'aiocache>=0.10', 'apscheduler', 'sqlalchemy', 'baidu-aip', 'jieba', 'requests'],
     python_requires='>=3.6',
     platforms='any',
     classifiers=(
