99bcf5312a682b016e2b08a8ba02127be53d428f
SincereXIA <937734121@qq.com>
2018-08-23 17:12:47 +0800

    fix setup

--- IAI/bot.py
+++ IAI/bot.py
@@ -9,4 +9,4 @@ if __name__ == '__main__':
     none.load_builtin_plugins()
     none.load_plugins(path.join(path.dirname(__file__),'iai','plugins'),
                       'iai.plugins')
-    none.run(host='127.0.0.1', port=8082)
+    none.run()
