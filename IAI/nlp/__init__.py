964fd1837aeb959f8183acf6a8229bedfef15ecd
SincereXIA <937734121@qq.com>
2018-08-28 13:25:53 +0800

    'fix'

--- IAI/nlp/__init__.py
+++
@@ -1,8 +0,0 @@
-from aip import AipNlp
-from IAI.setup import *
-APP_ID = str(11705272)
-API_KEY = str(BD_API_KEY)
-SECRET_KEY = str(BD_SECRET_KEY)
-
-client = AipNlp(APP_ID, API_KEY, SECRET_KEY)
-
--- IAI/nlp/datetime_nlp.py
+++
@@ -1,3 +0,0 @@
-import datetime
-async def date_nlp(text):
-    return datetime.date(year=datetime.datetime.now().year, month=8, day=27)
