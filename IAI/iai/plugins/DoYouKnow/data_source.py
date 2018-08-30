1dab1ee9bf5f912a86d03c466c557534a3b1acc1
SincereXIA <937734121@qq.com>
2018-08-30 21:55:58 +0800

    修正 doyouknow
    Signed-off-by: SincereXIA <937734121@qq.com>

--- IAI/iai/plugins/DoYouKnow/data_source.py
+++ IAI/iai/plugins/DoYouKnow/data_source.py
@@ -1,8 +1,9 @@
 from sqlalchemy import func, Integer, Column, TEXT, String, create_engine, Time, VARCHAR,Date
 from sqlalchemy.ext.declarative import declarative_base
 from IAI.DBdriver import DBSession
-
-
+import requests
+import json
+import random
 # 创建对象的基类:
 Base = declarative_base()
 # 定义User对象:
@@ -17,12 +18,26 @@ class DoYouKnow(Base):
     ad_flag = Column(Integer)
 
 async def get_do_you_know():
-    session = DBSession()
-    dyn = session.query(DoYouKnow).order_by(func.rand()).first()
-    dyn.times += 1
-    session.merge(dyn)
-    session.close()
+    if random.randint(1,3) == 3:
+        session = DBSession()
+        dyn = session.query(DoYouKnow).order_by(func.rand()).first()
+        dyn.times += 1
+        session.merge(dyn)
+        session.close()
+        result = {}
+        result['text'] = dyn.text
+        result['info'] = dyn.times
+    else:
+        result = get_one_content()
+    return result
+
+async def get_one_content():
+    idlist = requests.get('http://v3.wufazhuce.com:8000/api/onelist/idlist')
+    idlist = json.loads(idlist.text)
+    id = int(idlist['data'][0])
+    content = requests.get(f'http://v3.wufazhuce.com:8000/api/onelist/{id-random.randint(0,300)}/0')
+    content = json.loads(content.text)
     result = {}
-    result['text'] = dyn.text
-    result['info'] = dyn.times
+    result['text'] = content['data']['content_list'][0]['forward']
+    result['info'] = content['data']['content_list'][0]['words_info']
     return result
